23806fcfad3817c5f158a3b3e946c7d6a76f883a
chor <cfm77777@163.com>
2025-02-24 20:45:09 +0800

    chore: md

--- src/hooks/editor.tsx
+++ src/hooks/editor.tsx
@@ -4,7 +4,7 @@ import { StyleContext } from "../contexts/StyleContext";
 import { ThemeContext } from "../contexts/ThemeContext";
 import { TerminalContext } from "../contexts/TerminalContext";
 
-import Utils from "../common/Utils"
+import Utils from "../common/Utils";
 
 export const useEditorInput = (
   consoleFocused: boolean,
@@ -18,20 +18,21 @@ export const useEditorInput = (
   enableInput: boolean,
   commands: any
 ) => {
-  const { getPreviousCommand, getNextCommand } = React.useContext(TerminalContext);
+  const { getPreviousCommand, getNextCommand } =
+    React.useContext(TerminalContext);
 
   const handleKeyDownEvent = (event: any) => {
     if (!consoleFocused) {
       return;
     }
-    //checks the value of enableInput and returns if its false
-    if(!enableInput){
+    // checks the value of enableInput and returns if its false
+    if (!enableInput) {
       return;
     }
     event.preventDefault();
 
     const eventKey = event.key;
-    
+
     if (eventKey === "Enter") {
       setProcessCurrentLine(true);
       return;
@@ -40,9 +41,13 @@ export const useEditorInput = (
     let nextInput = null;
 
     if (eventKey === "Backspace") {
-      const [caretTextBefore, caretTextAfter] = Utils.splitStringAtIndex(editorInput, caretPosition);
+      const [caretTextBefore, caretTextAfter] = Utils.splitStringAtIndex(
+        editorInput,
+        caretPosition
+      );
       nextInput = caretTextBefore.slice(0, -1) + caretTextAfter;
-      if (editorInput && editorInput.length !== 0) setCaretPosition(caretPosition - 1);
+      if (editorInput && editorInput.length !== 0)
+        setCaretPosition(caretPosition - 1);
     } else if (eventKey === "ArrowUp") {
       nextInput = getPreviousCommand();
       if (nextInput) setCaretPosition(nextInput.length);
@@ -52,39 +57,74 @@ export const useEditorInput = (
       else setCaretPosition(0);
     } else if (eventKey === "ArrowLeft") {
       if (caretPosition > 0) setCaretPosition(caretPosition - 1);
-      nextInput = editorInput
+      nextInput = editorInput;
     } else if (eventKey === "ArrowRight") {
-      if (caretPosition < editorInput.length) setCaretPosition(caretPosition + 1);
-      nextInput = editorInput
-    } else if ( eventKey === "Tab"){
-      const command = Object.keys(commands).find((command : string)=>command.substring(0,editorInput.length) === editorInput);
-      if(command){
-        setEditorInput(command);
-        setCaretPosition(command.length)
-        return
-      }
-    } else if ((event.metaKey || event.ctrlKey) && eventKey.toLowerCase() === "v") {
-      navigator.clipboard.readText()
-      .then(pastedText => {
-        const [caretTextBefore, caretTextAfter] = Utils.splitStringAtIndex(editorInput || "", caretPosition);
-        nextInput = caretTextBefore + pastedText + caretTextAfter;
-        setCaretPosition(caretPosition + pastedText.length);
-        setEditorInput(nextInput);
-      });
-    } else if ((event.metaKey || event.ctrlKey) && eventKey.toLowerCase() === "c") {
-      const selectedText = window.getSelection().toString();
-      navigator.clipboard.writeText(selectedText)
-      .then(() => {
-        nextInput = editorInput;
-        setEditorInput(nextInput);
-      });
-    } else {
-      if (eventKey && eventKey.length === 1) {
-        const [caretTextBefore, caretTextAfter] = Utils.splitStringAtIndex(editorInput, caretPosition);
-        nextInput = caretTextBefore + eventKey + caretTextAfter;
+      if (caretPosition < editorInput.length)
         setCaretPosition(caretPosition + 1);
-      } else nextInput = editorInput
-    }
+      nextInput = editorInput;
+    } else if (eventKey === "Tab") {
+      const command = Object.keys(commands).find(
+        (command: string) =>
+          command.substring(0, editorInput.length) === editorInput
+      );
+      if (command) {
+        setEditorInput(command);
+        setCaretPosition(command.length);
+        return;
+      }
+    } else if (
+      (event.metaKey || event.ctrlKey) &&
+      eventKey.toLowerCase() === "v"
+    ) {
+      navigator.clipboard
+        .readText()
+        .then((pastedText) => {
+          const [caretTextBefore, caretTextAfter] = Utils.splitStringAtIndex(
+            editorInput || "",
+            caretPosition
+          );
+          nextInput = caretTextBefore + pastedText + caretTextAfter;
+          setCaretPosition(caretPosition + pastedText.length);
+          setEditorInput(nextInput);
+        })
+        .catch((error) => {
+          const textArea = document.createElement("textarea");
+          document.body.appendChild(textArea);
+          textArea.focus();
+          document.execCommand("paste");
+          const pastedText = textArea.value;
+          document.body.removeChild(textArea);
+          return pastedText;
+        });
+    } else if (
+      (event.metaKey || event.ctrlKey) &&
+      eventKey.toLowerCase() === "c"
+    ) {
+      const selectedText = window.getSelection().toString();
+      navigator.clipboard
+        .writeText(selectedText)
+        .then(() => {
+          nextInput = editorInput;
+          setEditorInput(nextInput);
+        })
+        .catch((error) => {
+          console.log(error);
+
+          const textArea = document.createElement("textarea");
+          textArea.value = selectedText;
+          document.body.appendChild(textArea);
+          textArea.select();
+          document.execCommand("copy");
+          document.body.removeChild(textArea);
+        });
+    } else if (eventKey && eventKey.length === 1) {
+      const [caretTextBefore, caretTextAfter] = Utils.splitStringAtIndex(
+        editorInput,
+        caretPosition
+      );
+      nextInput = caretTextBefore + eventKey + caretTextAfter;
+      setCaretPosition(caretPosition + 1);
+    } else nextInput = editorInput;
 
     setEditorInput(nextInput);
     setProcessCurrentLine(false);
@@ -99,14 +139,14 @@ export const useEditorInput = (
     };
   });
 
-  React.useEffect(
-    () => {
-      const [caretTextBefore, caretTextAfter] = Utils.splitStringAtIndex(editorInput, caretPosition);
-      setBeforeCaretText(caretTextBefore);
-      setAfterCaretText(caretTextAfter);
-    },
-    [editorInput, caretPosition]
-  );
+  React.useEffect(() => {
+    const [caretTextBefore, caretTextAfter] = Utils.splitStringAtIndex(
+      editorInput,
+      caretPosition
+    );
+    setBeforeCaretText(caretTextBefore);
+    setAfterCaretText(caretTextAfter);
+  }, [editorInput, caretPosition]);
 };
 
 export const useBufferedContent = (
@@ -122,89 +162,90 @@ export const useBufferedContent = (
   errorMessage: any,
   defaultHandler: any
 ) => {
-  const { bufferedContent, setBufferedContent, setTemporaryContent } = React.useContext(TerminalContext);
+  const { bufferedContent, setBufferedContent, setTemporaryContent } =
+    React.useContext(TerminalContext);
   const style = React.useContext(StyleContext);
   const themeStyles = React.useContext(ThemeContext);
 
-  React.useEffect(
-    () => {
-      if (!processCurrentLine) {
-        return;
-      }
+  React.useEffect(() => {
+    if (!processCurrentLine) {
+      return;
+    }
 
-      const processCommand = async (text: string) => {
+    const processCommand = async (text: string) => {
+      const [command, ...rest] = text.trim().split(" ");
+      let output = "";
 
-        const [command, ...rest] = text.trim().split(" ");
-        let output = "";
-
-        if(command === "clear") {
-          setBufferedContent("");
-          setCurrentText("");
-          setProcessCurrentLine(false);
-          setCaretPosition(0);
-          setBeforeCaretText("");
-          setAfterCaretText("");
-          return
-        }
-
-        const waiting = (
-          <>
-            <span style={{ color: themeStyles.themePromptColor }}>{prompt} </span>
-            <span>{currentText}</span>
-            <br />
-          </>
-        );
-        setBufferedContent((previous: React.ReactNode) => (<>
-          {previous}
-          {waiting}
-        </>));
+      if (command === "clear") {
+        setBufferedContent("");
         setCurrentText("");
+        setProcessCurrentLine(false);
         setCaretPosition(0);
         setBeforeCaretText("");
         setAfterCaretText("");
+        return;
+      }
 
-        if (text) {
-          const commandArguments = rest.join(" ");
+      const waiting = (
+        <>
+          <span style={{ color: themeStyles.themePromptColor }}>{prompt} </span>
+          <span>{currentText}</span>
+          <br />
+        </>
+      );
+      setBufferedContent((previous: React.ReactNode) => (
+        <>
+          {previous}
+          {waiting}
+        </>
+      ));
+      setCurrentText("");
+      setCaretPosition(0);
+      setBeforeCaretText("");
+      setAfterCaretText("");
 
-          if (command && commands[command]) {
-            const executor = commands[command];
+      if (text) {
+        const commandArguments = rest.join(" ");
 
-            if (typeof executor === "function") {
-              output = await executor(commandArguments);
-            } else {
-              output = executor;
-            }
-          } else if (typeof defaultHandler === "function") {
-            output = await defaultHandler(command, commandArguments);
-          } else if (typeof errorMessage === "function") {
-            output = await errorMessage(command, commandArguments);
+        if (command && commands[command]) {
+          const executor = commands[command];
+
+          if (typeof executor === "function") {
+            output = await executor(commandArguments);
           } else {
-            output = errorMessage;
+            output = executor;
           }
+        } else if (typeof defaultHandler === "function") {
+          output = await defaultHandler(command, commandArguments);
+        } else if (typeof errorMessage === "function") {
+          output = await errorMessage(command, commandArguments);
+        } else {
+          output = errorMessage;
         }
-        const nextBufferedContent = (
-          <>
-            {output ? (
-              <span>
-                {output}
-                <br/>
-              </span>
-            ) : null}
-          </>
-        );
+      }
+      const nextBufferedContent = (
+        <>
+          {output ? (
+            <pre>
+              {output}
+              <br />
+            </pre>
+          ) : null}
+        </>
+      );
 
-        setBufferedContent((previousBufferedContent: React.ReactNode) => (<>
+      setBufferedContent((previousBufferedContent: React.ReactNode) => (
+        <>
           {previousBufferedContent}
           {nextBufferedContent}
-        </>));
-        setTemporaryContent("");
-        setProcessCurrentLine(false);
-      };
+        </>
+      ));
+      setTemporaryContent("");
+      setProcessCurrentLine(false);
+    };
 
-      processCommand(currentText);
-    },
-    [processCurrentLine]
-  );
+    processCommand(currentText);
+  }, [processCurrentLine]);
 };
 
 export const useCurrentLine = (
@@ -219,7 +260,8 @@ export const useCurrentLine = (
 ) => {
   const style = React.useContext(StyleContext);
   const themeStyles = React.useContext(ThemeContext);
-  const { appendCommandToHistory, temporaryContent } = React.useContext(TerminalContext);
+  const { appendCommandToHistory, temporaryContent } =
+    React.useContext(TerminalContext);
   const mobileInputRef = React.useRef(null);
   const [editorInput, setEditorInput] = React.useState("");
   const [processCurrentLine, setProcessCurrentLine] = React.useState(false);
@@ -227,48 +269,42 @@ export const useCurrentLine = (
   const [beforeCaretText, setBeforeCaretText] = React.useState("");
   const [afterCaretText, setAfterCaretText] = React.useState("");
 
-  React.useEffect(
-    () => {
-      if (!Utils.isMobile()) {
-        return;
-      }
-    },
-    [consoleFocused]
-  );
-
-  React.useEffect(
-    () => {
-      if (!processCurrentLine) {
-        return;
-      }
-      appendCommandToHistory(editorInput);
-    },
-    [processCurrentLine]
-  );
+  React.useEffect(() => {
+    if (!Utils.isMobile()) {
+    }
+  }, [consoleFocused]);
 
   React.useEffect(() => {
-    if(wrapperRef.current !== null && mobileInputRef.current !== null) {
+    if (!processCurrentLine) {
+      return;
+    }
+    appendCommandToHistory(editorInput);
+  }, [processCurrentLine]);
+
+  React.useEffect(() => {
+    if (wrapperRef.current !== null && mobileInputRef.current !== null) {
       wrapperRef.current.onclick = () => {
         mobileInputRef.current.focus();
-      }
+      };
     }
-  },[])
+  }, []);
 
-  const mobileInput = Utils.isMobile() && enableInput? (
-    <div className={style.mobileInput}>
-      <input
-        type="text"
-        autoComplete="off"
-        autoCorrect="off"
-        autoCapitalize="off"
-        spellCheck="false"
-        value={editorInput}
-        onChange={(event) => setEditorInput(event.target.value)}
-        ref={mobileInputRef}
-        data-testid={"editor-input"}
-      />
-    </div>
-  ) : null;
+  const mobileInput =
+    Utils.isMobile() && enableInput ? (
+      <div className={style.mobileInput}>
+        <input
+          type="text"
+          autoComplete="off"
+          autoCorrect="off"
+          autoCapitalize="off"
+          spellCheck="false"
+          value={editorInput}
+          onChange={(event) => setEditorInput(event.target.value)}
+          ref={mobileInputRef}
+          data-testid="editor-input"
+        />
+      </div>
+    ) : null;
 
   const currentLine = !processCurrentLine ? (
     <>
@@ -276,9 +312,12 @@ export const useCurrentLine = (
       <span style={{ color: themeStyles.themePromptColor }}>{prompt}</span>
       <div className={style.lineText}>
         <span className={style.preWhiteSpace}>{beforeCaretText}</span>
-        {consoleFocused && caret ? (  //if caret isn't true, caret won't be displayed
+        {consoleFocused && caret ? ( // if caret isn't true, caret won't be displayed
           <span className={style.caret}>
-            <span className={style.caretAfter} style={{ background: themeStyles.themeColor }} />
+            <span
+              className={style.caretAfter}
+              style={{ background: themeStyles.themeColor }}
+            />
           </span>
         ) : null}
         <span className={style.preWhiteSpace}>{afterCaretText}</span>
@@ -288,9 +327,12 @@ export const useCurrentLine = (
     <>
       {mobileInput}
       <div className={style.lineText}>
-        {consoleFocused && caret? ( //if caret isn't true, caret won't be displayed
+        {consoleFocused && caret ? ( // if caret isn't true, caret won't be displayed
           <span className={style.caret}>
-            <span className={style.caretAfter} style={{ background: themeStyles.themeColor }} />
+            <span
+              className={style.caretAfter}
+              style={{ background: themeStyles.themeColor }}
+            />
           </span>
         ) : null}
         <span className={style.preWhiteSpace}>{temporaryContent}</span>
@@ -329,12 +371,9 @@ export const useCurrentLine = (
 };
 
 export const useScrollToBottom = (changesToWatch: any, wrapperRef: any) => {
-  React.useEffect(
-    () => {
-      if (!wrapperRef.current) return;
-      // eslint-disable-next-line no-param-reassign
-      wrapperRef.current.scrollTop = wrapperRef.current.scrollHeight;
-    },
-    [changesToWatch]
-  );
+  React.useEffect(() => {
+    if (!wrapperRef.current) return;
+    // eslint-disable-next-line no-param-reassign
+    wrapperRef.current.scrollTop = wrapperRef.current.scrollHeight;
+  }, [changesToWatch]);
 };
